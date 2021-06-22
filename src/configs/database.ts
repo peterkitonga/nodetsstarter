c0fbf97c2272aebcfefc8865e28ad5668fa10ee4
Peter Kitonga <pkitonga.pk@gmail.com>
2021-06-22 16:43:43 +0300

    Add configuration file for the mongodb connection

---
+++ src/configs/database.ts
@@ -0,0 +1,21 @@
+import { connect } from 'mongoose';
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_HOST = process.env.MONGO_HOST;
+const MONGO_DATABASE = process.env.MONGO_DATABASE;
+
+const mongodbUri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+
+interface MongoResponse {
+    status: string; 
+    message: string;
+}
+
+export const mongoConnect = (): Promise<MongoResponse> => {
+    return new Promise((resolve, reject) => {
+        connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+        .then(() => resolve({ status: 'success', message: 'MONGO CONNECTED!' }))
+        .catch(err => reject({ status: 'error', message: err.message }));
+    });
+};
