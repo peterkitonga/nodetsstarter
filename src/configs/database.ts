b2ff2bc5666d49d027df9671dfb097787fb38a45
Peter Kitonga <pkitonga.pk@gmail.com>
2021-06-22 18:57:16 +0300

    Import database interface to the database config file

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,4 +1,5 @@
 import { connect } from 'mongoose';
+import { ConnectionResponse } from '../interfaces/database';
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
@@ -7,10 +8,7 @@ const MONGO_DATABASE = process.env.MONGO_DATABASE;
 
 const mongodbUri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
 
-interface ConnectionResponse {
-    status: string; 
-    message: string;
-}
+
 
 export const mongoConnect = (): Promise<ConnectionResponse> => {
     return new Promise((resolve, reject) => {
