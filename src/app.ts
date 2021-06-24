8438aa68d5f894bc975da6ff526ca494d814c2ce
Peter Kitonga <pkitonga.pk@gmail.com>
2021-06-24 18:48:38 +0300

    Lint main server file

--- src/app.ts
+++ src/app.ts
@@ -11,32 +11,32 @@ app.use(express.json());
 app.use(express.static(public_path()));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+
+  next();
 });
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ message: `Hello There! Running at: ${process.env.APP_BASE_URL}` });
+  res.status(200).json({ message: `Hello There! Running at: ${process.env.APP_BASE_URL}` });
 });
 
 app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
-    const { status_code, message, data } = err;
-    let code = status_code || 500;
+  const { status_code, message, data } = err;
+  const code = status_code || 500;
 
-    res.status(code).json({ status: 'error', message, data });
+  res.status(code).json({ status: 'error', message, data });
 });
 
 (async () => {
-    const { status, message } = await mongoConnect();
-    
-    if (status === 'success') {
-        app.listen(port);
-        console.log(message);
-    } else {
-        console.log(message);
-        process.exit(1);
-    }
+  const { status, message } = await mongoConnect();
+
+  if (status === 'success') {
+    app.listen(port);
+    console.log(message);
+  } else {
+    console.log(message);
+    process.exit(1);
+  }
 })();
