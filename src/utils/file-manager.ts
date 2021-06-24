2ac5d390345116be70b821f993e59340bcdc52dd
Peter Kitonga <pkitonga.pk@gmail.com>
2021-06-24 21:28:14 +0300

    Add util for managing file storage

---
+++ src/utils/file-manager.ts
@@ -0,0 +1,46 @@
+import { relative, dirname } from 'path';
+import { unlink, symlink, createWriteStream } from 'fs';
+
+import { storage_path } from '../utils/path';
+import { CustomResponse } from '../interfaces/responses';
+
+export const storeLocalFile = (filename: string): Promise<CustomResponse> => {
+  const filepath = storage_path(`app/public/${filename}`);
+
+  return new Promise((resolve, reject) => {
+    createWriteStream(filepath)
+      .on('error', (err) => reject({ status: 'error', message: err.message }))
+      .on('finish', () => {
+        /**
+         * Create symlink to the public folder
+         *
+         * @link https://stackoverflow.com/questions/29777506/create-relative-symlinks-using-absolute-paths-in-node-js#answer-57281866
+         */
+        const source = `/storage/app/public/${filename}`;
+        const absoluteTarget = `/public/storage/${filename}`;
+        const target = relative(dirname(source), absoluteTarget);
+
+        symlink(target, source, (err) => {
+          if (err) {
+            reject({ status: 'error', message: err.message });
+          } else {
+            resolve({ status: 'success', data: filepath });
+          }
+        });
+      });
+  });
+};
+
+export const deleteLocalFile = (filename: string): Promise<CustomResponse> => {
+  const filepath = storage_path(`app/public/${filename}`);
+
+  return new Promise((resolve, reject) => {
+    unlink(filepath, (err) => {
+      if (err) {
+        reject({ status: 'error', message: err.message });
+      } else {
+        resolve({ status: 'success', data: filepath });
+      }
+    });
+  });
+};
