834fe5b841761c5a1334653e35775ce3daecf427
Peter Kitonga <pkitonga.pk@gmail.com>
2021-06-24 18:01:45 +0300

    Add check for the mongo provider in the database config

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,12 +1,19 @@
 import { connect } from 'mongoose';
 import { ConnectionResponse } from '../interfaces/database';
 
+const MONGO_HOST = process.env.MONGO_HOST;
+const MONGO_PORT = process.env.MONGO_PORT;
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_HOST = process.env.MONGO_HOST;
 const MONGO_DATABASE = process.env.MONGO_DATABASE;
 
-const mongodbUri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+let mongodbUri: string;
+
+if (process.env.MONGO_PROVIDER === 'atlas') {
+    mongodbUri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+} else {
+    mongodbUri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+}
 
 export const mongoConnect = (): Promise<ConnectionResponse> => {
     return new Promise((resolve, reject) => {
