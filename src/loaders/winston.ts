678c777f8ea031aa342665ffe07bce0ea982238b
Peter Kitonga <pkitonga.pk@gmail.com>
2021-07-01 13:50:02 +0300

    Modify the logging logic

--- src/loaders/winston.ts
+++ src/loaders/winston.ts
@@ -1,14 +1,29 @@
-import winston from 'winston';
+import { createLogger, format, transports } from 'winston';
+
+import { storagePath } from '../utils/path';
 
 import configs from '../configs';
 
-export const logger = winston.createLogger({
-  levels: winston.config.syslog.levels,
-  transports: [
-    new winston.transports.Console({ level: 'error' }),
-    new winston.transports.File({
-      filename: 'combined.log',
-      level: configs.logging.level,
+export const logger = createLogger({
+  level: configs.logging.level,
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
     }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json(),
+  ),
+  transports: [
+    new transports.File({ filename: storagePath('logs/error.log'), level: 'error' }),
+    new transports.File({ filename: storagePath('logs/combined.log') }),
   ],
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    }),
+  );
+}
