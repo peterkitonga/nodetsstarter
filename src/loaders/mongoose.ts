738e8589f198f98b7cbc6a0d0b8b5d804f9a5b9c
Peter Kitonga <pkitonga.pk@gmail.com>
2021-07-02 19:05:41 +0300

    Refactor mongoose and winston loaders into classes

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -1,12 +1,29 @@
 import { connect } from 'mongoose';
-import { ConnectionResponse } from '../interfaces/database';
 
 import configs from '../configs';
+import { ConnectionResponse } from '../interfaces/database';
 
-export const mongoConnect = (): Promise<ConnectionResponse> => {
-  return new Promise((resolve, reject) => {
-    connect(configs.database.uri(), { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-      .then(() => resolve({ status: 'success', message: 'MONGO CONNECTED!' }))
-      .catch((err) => reject({ status: 'error', message: err.message }));
-  });
-};
+export default class MongooseConnect {
+  private static instance: MongooseConnect;
+  private uri: string;
+
+  private constructor() {
+    this.uri = configs.database.uri();
+  }
+
+  public static init(): MongooseConnect {
+    if (!this.instance) {
+      this.instance = new MongooseConnect();
+    }
+
+    return this.instance;
+  }
+
+  public connect(): Promise<ConnectionResponse> {
+    return new Promise((resolve, reject) => {
+      connect(this.uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+        .then(() => resolve({ status: 'success', message: 'MONGO CONNECTED!' }))
+        .catch((err) => reject({ status: 'error', message: err.message }));
+    });
+  }
+}
--- src/loaders/winston.ts
+++ src/loaders/winston.ts
@@ -1,29 +1,52 @@
-import { createLogger, format, transports } from 'winston';
-
-import { storagePath } from '../utils/path';
+import { createLogger, format, Logger, transports } from 'winston';
 
 import configs from '../configs';
+import { storagePath } from '../utils/path';
 
-export const logger = createLogger({
-  level: configs.logging.level,
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json(),
-  ),
-  transports: [
-    new transports.File({ filename: storagePath('logs/error.log'), level: 'error' }),
-    new transports.File({ filename: storagePath('logs/combined.log') }),
-  ],
-});
+export default class WinstonLogger {
+  private static instance: WinstonLogger;
+  private level: string = configs.logging.level;
+  private logger: Logger;
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    }),
-  );
+  private constructor() {
+    this.logger = createLogger({
+      level: this.level,
+      format: format.combine(
+        format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json(),
+      ),
+      transports: [
+        new transports.File({ filename: storagePath('logs/error.log'), level: 'error' }),
+        new transports.File({ filename: storagePath('logs/combined.log') }),
+      ],
+    });
+
+    if (process.env.NODE_ENV !== 'production') {
+      this.logger.add(
+        new transports.Console({
+          format: format.combine(format.colorize(), format.simple()),
+        }),
+      );
+    }
+  }
+
+  public static init(): WinstonLogger {
+    if (!this.instance) {
+      this.instance = new WinstonLogger();
+    }
+
+    return this.instance;
+  }
+
+  public info(message: string): void {
+    this.logger.info(message);
+  }
+
+  public error(message: string): void {
+    this.logger.error(message);
+  }
 }
