0f169977616f60732fbd6c73a65b888f2c117a5f
Peter Kitonga <pkitonga.pk@gmail.com>
2021-07-22 19:56:41 +0300

    Add custom error classes

---
+++ src/common/errors/unauthorized.ts
@@ -0,0 +1,8 @@
+import BaseError from './base';
+import { HttpStatusCodes } from '../enums/http';
+
+export default class UnauthorizedError extends BaseError {
+  public constructor(public message: string) {
+    super('UnauthorizedError', HttpStatusCodes.UNAUTHORIZED, true, message);
+  }
+}
