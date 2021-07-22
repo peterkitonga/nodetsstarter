e772d86a748f35ed9955c6956150ed978a68c343
Peter Kitonga <pkitonga.pk@gmail.com>
2021-07-22 21:14:31 +0300

    Add interface for the token response after valid authentication

--- src/common/interfaces/responses.ts
+++ src/common/interfaces/responses.ts
@@ -1,5 +1,13 @@
+import { UserModel } from './database';
+
 export interface ResultResponse<DataType> {
   status: string; // possible values: success, error, warning, info
   message?: string;
   data?: DataType;
+}
+
+export interface TokenResponse {
+  token: string;
+  lifetime: string;
+  auth: UserModel;
 }
