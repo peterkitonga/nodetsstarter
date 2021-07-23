1787905b11c8b8fb2deaac13c1e19105ab5974f0
Peter Kitonga <pkitonga.pk@gmail.com>
2021-07-23 19:15:43 +0300

    Modify loader classes for mongoose and winston

--- src/loaders/winston.ts
+++ src/loaders/winston.ts
@@ -3,34 +3,36 @@ import { createLogger, format, Logger, transports } from 'winston';
 import configs from '../configs';
 import { storagePath } from '../utils/path';
 
-export default class WinstonLogger {
-  private static logger: Logger = createLogger({
-    level: configs.logging.level,
-    format: format.combine(
-      format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
-      }),
-      format.errors({ stack: true }),
-      format.splat(),
-      format.json(),
-    ),
-    transports: [
-      new transports.File({ filename: storagePath('logs/error.log'), level: 'error' }),
-      process.env.NODE_ENV !== 'production'
-        ? new transports.Console({ format: format.combine(format.colorize(), format.simple()) })
-        : new transports.File({ filename: storagePath('logs/combined.log') }),
-    ],
-  });
+class WinstonLogger {
+  private logger: Logger;
 
   public constructor() {
-    // constructor
+    this.logger = createLogger({
+      level: configs.logging.level,
+      format: format.combine(
+        format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json(),
+      ),
+      transports: [
+        new transports.File({ filename: storagePath('logs/error.log'), level: 'error' }),
+        process.env.NODE_ENV !== 'production'
+          ? new transports.Console({ format: format.combine(format.colorize(), format.simple()) })
+          : new transports.File({ filename: storagePath('logs/combined.log') }),
+      ],
+    });
   }
 
-  public static info(message: string): void {
+  public info(message: string): void {
     this.logger.info(message);
   }
 
-  public static error(message: string): void {
+  public error(message: string): void {
     this.logger.error(message);
   }
 }
+
+export default new WinstonLogger();
