6e724fcef1befa58959d2a4a5a662d92634fdee9
Peter Kitonga <pkitonga.pk@gmail.com>
2021-07-27 12:02:05 +0300

    Move database connection logic to a separate function

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -22,7 +22,7 @@ class ExpressApp {
     //
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupBodyParser();
     this.serveStaticFiles();
 
@@ -33,33 +33,39 @@ class ExpressApp {
     this.handleAppRoutes();
     this.handleNonExistingRoute();
     this.handleErrorMiddleware();
+
+    await this.connectDatabase();
     this.listen();
   }
 
-  public async listen(): Promise<void> {
+  public listen(): void {
+    const server = this.app.listen(configs.app.port);
+    const gracefulShutdown = () => {
+      return server.close(async () => {
+        const { message } = await MongooseConnect.disconnect();
+        WinstonLogger.info('Server shutdown successfully!');
+        WinstonLogger.info(message!);
+      });
+    };
+
+    WinstonLogger.info(`Listening on: ${configs.app.base}, PID: ${process.pid}`);
+
+    process.on('SIGTERM', () => {
+      WinstonLogger.info('Starting graceful shutdown of server...');
+      gracefulShutdown();
+    });
+
+    process.on('SIGINT', () => {
+      WinstonLogger.info('Exiting server process cleanly...');
+      gracefulShutdown();
+    });
+  }
+
+  public async connectDatabase(): Promise<void> {
     try {
-      const server = this.app.listen(configs.app.port);
       const { message } = await MongooseConnect.connect();
-      const gracefulShutdown = () => {
-        return server.close(async () => {
-          const { message } = await MongooseConnect.disconnect();
-          WinstonLogger.info('Server shutdown successfully!');
-          WinstonLogger.info(message!);
-        });
-      };
 
-      WinstonLogger.info(`Listening on: ${configs.app.base}, PID: ${process.pid}`);
       WinstonLogger.info(message!);
-
-      process.on('SIGTERM', () => {
-        WinstonLogger.info('Starting graceful shutdown of server...');
-        gracefulShutdown();
-      });
-
-      process.on('SIGINT', () => {
-        WinstonLogger.info('Exiting server process cleanly...');
-        gracefulShutdown();
-      });
     } catch (err) {
       WinstonLogger.error(err.message);
     }
