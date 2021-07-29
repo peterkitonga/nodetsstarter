08f6e8b1b6515c01fa0198e67b6eaa2f1af3b20a
Peter Kitonga <pkitonga.pk@gmail.com>
2021-07-29 12:01:36 +0300

    Add method for getting view data to the nodemailer loader class

--- src/loaders/nodemailer.ts
+++ src/loaders/nodemailer.ts
@@ -6,60 +6,77 @@ import configs from '../configs';
 import { viewPath } from '../utils/path';
 import { ResultResponse } from '../common/interfaces/responses';
 
-export default class Mailer {
-  /**
-   * Required type casting: SMTPTransport.Options,
-   * otherwise will default to TransportOptions type
-   *
-   * @link https://github.com/DefinitelyTyped/DefinitelyTyped/issues/35847#issuecomment-638091133
-   */
-  private static transporter: Transporter = createTransport(<SMTPTransport.Options>{
-    host: configs.mail.host,
-    port: configs.mail.port,
-    secure: configs.mail.encryption, // ssl
-    auth: {
-      user: configs.mail.credentials.username,
-      pass: configs.mail.credentials.password,
-    },
-  });
+class Mailer {
+  private transporter: Transporter;
 
   public constructor() {
-    // constructor
+    /**
+     * Required type casting: SMTPTransport.Options,
+     * otherwise will default to TransportOptions type
+     *
+     * @link https://github.com/DefinitelyTyped/DefinitelyTyped/issues/35847#issuecomment-638091133
+     */
+    this.transporter = createTransport(<SMTPTransport.Options>{
+      host: configs.mail.host,
+      port: configs.mail.port,
+      secure: configs.mail.encryption, // ssl
+      auth: {
+        user: configs.mail.credentials.username,
+        pass: configs.mail.credentials.password,
+      },
+    });
   }
 
-  public static send(
+  public send(
     receiver: string,
     subject: string,
-    message_options: ejs.Data,
+    messageOptions: ejs.Data,
     view: string,
   ): Promise<ResultResponse<null>> {
     // send mail with defined transport object
+    return this.getViewData(view, messageOptions)
+      .then((viewResponse) => {
+        if (viewResponse.status === 'success') {
+          const mailOptions: SendMailOptions = {
+            from: `"${configs.mail.from.name}" ${configs.mail.from.address}`, // sender address (who sends)
+            to: receiver, // list of receivers (who receives)
+            subject: subject, // subject line
+            html: viewResponse.data, // html body
+          };
+
+          return this.transporter.sendMail(mailOptions);
+        } else {
+          throw new Error(viewResponse.message);
+        }
+      })
+      .then((mailResponse) => {
+        if ('response' in mailResponse) {
+          return Promise.resolve({ status: 'success', message: mailResponse.response });
+        } else {
+          throw mailResponse.error;
+        }
+      })
+      .catch((err) => {
+        return Promise.reject({ status: 'success', message: err.message });
+      });
+  }
+
+  private getViewData(view: string, dataOptions: ejs.Data): Promise<ResultResponse<string>> {
     return new Promise((resolve, reject) => {
       /**
        * Using ejs template for mail views
        *
        * @link https://stackoverflow.com/questions/41304922/sending-ejs-template-using-nodemailer#answer-41337102
        */
-      ejs.renderFile(viewPath(`emails/${view}.ejs`), message_options, (fileErr, data) => {
+      ejs.renderFile(viewPath(`emails/${view}.ejs`), dataOptions, (fileErr, data) => {
         if (fileErr) {
           reject({ status: 'error', message: fileErr.message });
         } else {
-          const mail_options: SendMailOptions = {
-            from: `"${configs.mail.from.name}" ${configs.mail.from.address}`, // sender address (who sends)
-            to: receiver, // list of receivers (who receives)
-            subject: subject, // subject line
-            html: data, // html body
-          };
-
-          this.transporter.sendMail(mail_options, (mailErr, info) => {
-            if (mailErr) {
-              reject({ status: 'error', message: mailErr.message });
-            } else {
-              resolve({ status: 'success', message: `Message sent: ${info.response}` });
-            }
-          });
+          resolve({ status: 'success', data: data });
         }
       });
     });
   }
 }
+
+export default new Mailer();
