e0b00b1cd02f2b22e90d75eb3e0cadbec7962940
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-02 11:57:29 +0300

    Add fields for s3 configurations to the file systems config file

--- src/configs/filesystems.ts
+++ src/configs/filesystems.ts
@@ -8,6 +8,12 @@ export default {
       dir: storagePath('app/public'),
       url: `${process.env.APP_BASE_URL}/storage`,
     },
-    s3: {},
+    s3: {
+      region: process.env.AWS_BUCKET_REGION,
+      bucket: process.env.AWS_BUCKET_NAME,
+      key: process.env.AWS_ACCESS_KEY_ID,
+      secret: process.env.AWS_ACCESS_SECRET,
+      owner: process.env.AWS_BUCKET_OWNER_ID,
+    },
   },
 };
