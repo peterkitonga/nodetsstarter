6b5a4cdeacf5d8e73657004269e117597924f822
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-02 23:34:53 +0300

    Add fixes for the file storage service class

--- src/services/file.ts
+++ src/services/file.ts
@@ -1,4 +1,4 @@
-import { unlink, symlink, writeFile } from 'fs';
+import { unlink, symlink, writeFile } from 'fs/promises';
 
 import configs from '../configs';
 import { publicPath } from '../utils/path';
@@ -12,7 +12,6 @@ export default class FileStorageService {
 
   public async storeFile(base64String: string): Promise<ResultResponse<string>> {
     try {
-      let response: ResultResponse<string>;
       const storageProvider = configs.filesystems.provider;
 
       const fileExt = base64String.substring(base64String.indexOf('/') + 1, base64String.indexOf(';base64'));
@@ -21,12 +20,10 @@ export default class FileStorageService {
       const fileName = `${Date.now()}.${fileExt}`;
 
       if (storageProvider === 'local') {
-        response = await this.storeLocalFile(fileName, base64File!);
+        return await this.storeLocalFile(fileName, base64File!);
       } else {
-        response = await this.storeCloudFile(fileName, base64File!, fileType);
+        return await this.storeCloudFile(fileName, base64File!, fileType);
       }
-
-      return response;
     } catch (err) {
       throw err;
     }
@@ -34,59 +31,45 @@ export default class FileStorageService {
 
   public async deleteFile(fileName: string): Promise<ResultResponse<string>> {
     try {
-      let response: ResultResponse<string>;
       const storageProvider = configs.filesystems.provider;
 
       if (storageProvider === 'local') {
-        response = await this.deleteLocalFile(fileName);
+        return await this.deleteLocalFile(fileName);
       } else {
-        response = await this.deleteCloudFile(fileName);
+        return await this.deleteCloudFile(fileName);
       }
-
-      return response;
     } catch (err) {
       throw err;
     }
   }
 
-  private storeLocalFile(fileName: string, base64File: string): Promise<ResultResponse<string>> {
-    const filePath = `${configs.filesystems.providers.local.dir}/${fileName}`;
+  private async storeLocalFile(fileName: string, base64File: string): Promise<ResultResponse<string>> {
+    try {
+      const filePath = `${configs.filesystems.providers.local.dir}/${fileName}`;
 
-    return new Promise((resolve, reject) => {
-      writeFile(filePath, base64File!, { encoding: 'base64' }, (err) => {
-        if (err) {
-          reject({ status: 'error', message: err.message });
-        } else {
-          const absoluteTarget = filePath;
-          const absolutePath = publicPath(`storage/${fileName}`);
+      const absoluteTarget = filePath;
+      const absolutePath = publicPath(`storage/${fileName}`);
 
-          /**
-           * Create symlink to the public/storage folder
-           */
-          symlink(absoluteTarget, absolutePath, (err) => {
-            if (err) {
-              reject({ status: 'error', message: err.message });
-            } else {
-              resolve({ status: 'success', data: `${configs.filesystems.providers.local.url}/${fileName}` });
-            }
-          });
-        }
-      });
-    });
+      await writeFile(filePath, base64File!, { encoding: 'base64' });
+      await symlink(absoluteTarget, absolutePath);
+
+      return { status: 'success', data: `${configs.filesystems.providers.local.url}/${fileName}` };
+    } catch (err) {
+      return { status: 'error', message: err.message };
+    }
   }
 
-  private deleteLocalFile(fileName: string): Promise<ResultResponse<string>> {
-    const filePath = `${configs.filesystems.providers.local.dir}/${fileName}`;
+  private async deleteLocalFile(fileName: string): Promise<ResultResponse<string>> {
+    try {
+      const filePath = `${configs.filesystems.providers.local.dir}/${fileName}`;
 
-    return new Promise((resolve, reject) => {
-      unlink(filePath, (err) => {
-        if (err) {
-          reject({ status: 'error', message: err.message });
-        } else {
-          resolve({ status: 'success', data: filePath });
-        }
-      });
-    });
+      await unlink(publicPath(`storage/${fileName}`));
+      await unlink(filePath);
+
+      return { status: 'success', data: `${configs.filesystems.providers.local.url}/${fileName}` };
+    } catch (err) {
+      return { status: 'error', message: err.message };
+    }
   }
 
   private storeCloudFile(fileName: string, base64File: string, fileType: string): Promise<ResultResponse<string>> {
@@ -95,13 +78,13 @@ export default class FileStorageService {
 
     return SThreeClient.saveToBucket(fileName, fileType, Buffer.from(base64File!, 'base64'))
       .then(() => {
-        return Promise.resolve({
+        return {
           status: 'success',
           data: `https://s3.${bucketRegion}.amazonaws.com/${bucketName}/${fileName}`,
-        });
+        };
       })
       .catch((err) => {
-        return Promise.reject({ status: 'error', message: err.message });
+        return { status: 'error', message: err.message };
       });
   }
 
@@ -111,13 +94,13 @@ export default class FileStorageService {
 
     return SThreeClient.deleteFromBucket(fileName)
       .then(() => {
-        return Promise.resolve({
+        return {
           status: 'success',
           data: `https://s3.${bucketRegion}.amazonaws.com/${bucketName}/${fileName}`,
-        });
+        };
       })
       .catch((err) => {
-        return Promise.reject({ status: 'error', message: err.message });
+        return { status: 'error', message: err.message };
       });
   }
 }
