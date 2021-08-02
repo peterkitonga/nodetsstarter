7fe01a71f2968bf7736a62eec6d0e1490b85d89d
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-02 11:59:05 +0300

    Add util functions for storing and deleting objects from an S3 bucket

--- src/utils/file-manager.ts
+++ src/utils/file-manager.ts
@@ -2,13 +2,11 @@ import { unlink, symlink, writeFile } from 'fs';
 
 import configs from '../configs';
 import { publicPath } from '../utils/path';
+import SThreeClient from '../loaders/aws-sthree';
 import { ResultResponse } from '../common/interfaces/responses';
 
-export const storeLocalFile = (base64String: string): Promise<ResultResponse<string>> => {
-  const fileExt = base64String.substring(base64String.indexOf('/') + 1, base64String.indexOf(';base64'));
-  const fileName = `${Date.now()}.${fileExt}`;
+const storeLocalFile = (fileName: string, base64File: string): Promise<ResultResponse<string>> => {
   const filePath = `${configs.filesystems.providers.local.dir}/${fileName}`;
-  const base64File = base64String.split(';base64,').pop();
 
   return new Promise((resolve, reject) => {
     writeFile(filePath, base64File!, { encoding: 'base64' }, (err) => {
@@ -33,7 +31,7 @@ export const storeLocalFile = (base64String: string): Promise<ResultResponse<str
   });
 };
 
-export const deleteLocalFile = (fileName: string): Promise<ResultResponse<string>> => {
+const deleteLocalFile = (fileName: string): Promise<ResultResponse<string>> => {
   const filePath = `${configs.filesystems.providers.local.dir}/${fileName}`;
 
   return new Promise((resolve, reject) => {
@@ -45,4 +43,75 @@ export const deleteLocalFile = (fileName: string): Promise<ResultResponse<string
       }
     });
   });
+};
+
+const storeCloudFile = (fileName: string, base64File: string, fileType: string): Promise<ResultResponse<string>> => {
+  const bucketRegion = configs.filesystems.providers.s3.region;
+  const bucketName = configs.filesystems.providers.s3.bucket;
+
+  return SThreeClient.saveToBucket(fileName, fileType, Buffer.from(base64File!, 'base64'))
+    .then(() => {
+      return Promise.resolve({
+        status: 'success',
+        data: `https://s3.${bucketRegion}.amazonaws.com/${bucketName}/${fileName}`,
+      });
+    })
+    .catch((err) => {
+      return Promise.reject({ status: 'error', message: err.message });
+    });
+};
+
+const deleteCloudFile = (fileName: string): Promise<ResultResponse<string>> => {
+  const bucketRegion = configs.filesystems.providers.s3.region;
+  const bucketName = configs.filesystems.providers.s3.bucket;
+
+  return SThreeClient.deleteFromBucket(fileName)
+    .then(() => {
+      return Promise.resolve({
+        status: 'success',
+        data: `https://s3.${bucketRegion}.amazonaws.com/${bucketName}/${fileName}`,
+      });
+    })
+    .catch((err) => {
+      return Promise.reject({ status: 'error', message: err.message });
+    });
+};
+
+export const storeFile = async (base64String: string): Promise<ResultResponse<string>> => {
+  try {
+    let response: ResultResponse<string>;
+    const storageProvider = configs.filesystems.provider;
+
+    const fileExt = base64String.substring(base64String.indexOf('/') + 1, base64String.indexOf(';base64'));
+    const fileType = base64String.substring('data:'.length, base64String.indexOf(';base64'));
+    const base64File = base64String.split(';base64,').pop();
+    const fileName = `${Date.now()}.${fileExt}`;
+
+    if (storageProvider === 'local') {
+      response = await storeLocalFile(fileName, base64File!);
+    } else {
+      response = await storeCloudFile(fileName, base64File!, fileType);
+    }
+
+    return response;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const deleteFile = async (fileName: string): Promise<ResultResponse<string>> => {
+  try {
+    let response: ResultResponse<string>;
+    const storageProvider = configs.filesystems.provider;
+
+    if (storageProvider === 'local') {
+      response = await deleteLocalFile(fileName);
+    } else {
+      response = await deleteCloudFile(fileName);
+    }
+
+    return response;
+  } catch (err) {
+    throw err;
+  }
 };
