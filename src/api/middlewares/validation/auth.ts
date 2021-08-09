95ed844060675cc79034310615c0f97f9c918598
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-09 19:54:02 +0300

    Add validation functionality for the reset password endpoint

--- src/api/middlewares/validation/auth.ts
+++ src/api/middlewares/validation/auth.ts
@@ -42,7 +42,7 @@ class AuthValidator {
           .any()
           .equal(joi.ref('password'))
           .required()
-          .label('password confirmation')
+          .label('password_confirmation')
           .options({ messages: { 'any.only': `The {#label} field should match the password` } }),
       });
 
@@ -108,6 +108,45 @@ class AuthValidator {
       });
 
       await resetSchema.validateAsync(request);
+
+      next();
+    } catch (err) {
+      const { message } = err.details[0];
+
+      next(new ValidationError(message, request));
+    }
+  }
+
+  public async resetPassword(
+    req: Request<unknown, unknown, ResetPasswordRequest>,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    const request = req.body;
+
+    try {
+      const resetPasswordSchema = joi.object({
+        token: joi.string().trim(true).required().label('token').messages({
+          'string.base': `The {#label} field should be text`,
+          'string.empty': `The {#label} field cannot be empty`,
+          'any.required': `The {#label} field is required`,
+        }),
+        password: joi.string().min(6).trim(true).required().label('password').messages({
+          'string.base': `The {#label} field should be text`,
+          'string.empty': `The {#label} field cannot be empty`,
+          'string.min': `The {#label} field should have a minimum length of {#limit} characters`,
+          'string.max': `The {#label} field should have a maximum length of {#limit} characters`,
+          'any.required': `The {#label} field is required`,
+        }),
+        password_confirmation: joi
+          .any()
+          .equal(joi.ref('password'))
+          .required()
+          .label('password_confirmation')
+          .options({ messages: { 'any.only': `The {#label} field should match the password` } }),
+      });
+
+      await resetPasswordSchema.validateAsync(request);
 
       next();
     } catch (err) {
