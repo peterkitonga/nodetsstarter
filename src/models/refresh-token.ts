b26bd0e77940d863fc2e07bad57a688fe978f48f
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-12 07:34:14 +0300

    Add missing collection names to refresh token & password reset models

--- src/models/refresh-token.ts
+++ src/models/refresh-token.ts
@@ -7,6 +7,7 @@ const schema = new Schema<RefreshTokenModel>(
     expires_at: { type: Date, required: true },
   },
   {
+    collection: 'refresh_tokens',
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
   },
 );
