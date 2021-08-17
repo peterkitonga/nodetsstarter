900d493c8c83dbfab352c8bea367339b559ea938
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-17 20:41:53 +0300

    Add bash script for the pull request auto merge

---
+++ travis-merge-script.sh
@@ -0,0 +1,13 @@
+if [ "$TRAVIS_BRANCH" != "develop" ]; then 
+    exit 0;
+fi
+
+export GIT_COMMITTER_EMAIL="$GITHUB_EMAIL"
+export GIT_COMMITTER_NAME="peterkitonga"
+
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+
+git fetch --all || exit
+git checkout master || exit
+git merge --no-ff -m "Merge pull request #${TRAVIS_PULL_REQUEST} from peterkitonga/${TRAVIS_BRANCH}" "$TRAVIS_COMMIT" || exit
+git push @github.com/">https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git"
