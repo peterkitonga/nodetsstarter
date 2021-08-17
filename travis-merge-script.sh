6affa2fa0dc714becf0c1d2f3edd6a383763daab
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-17 21:06:46 +0300

    Modify auto merge script

--- travis-merge-script.sh
+++ travis-merge-script.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 if [ "$TRAVIS_BRANCH" != "develop" ]; then 
     exit 0;
+else
+    export GIT_COMMITTER_EMAIL="$GITHUB_EMAIL"
+    export GIT_COMMITTER_NAME="peterkitonga"
+
+    git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+
+    git fetch --all || exit
+    git checkout master || exit
+    git merge --no-ff -m "Merge pull request #${TRAVIS_PULL_REQUEST} from peterkitonga/${TRAVIS_BRANCH}" "$TRAVIS_COMMIT" || exit
+    git push "https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git"
 fi
-
-export GIT_COMMITTER_EMAIL="$GITHUB_EMAIL"
-export GIT_COMMITTER_NAME="peterkitonga"
-
-git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
-
-git fetch --all || exit
-git checkout master || exit
-git merge --no-ff -m "Merge pull request #${TRAVIS_PULL_REQUEST} from peterkitonga/${TRAVIS_BRANCH}" "$TRAVIS_COMMIT" || exit
-git push "https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git"
