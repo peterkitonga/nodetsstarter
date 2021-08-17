0c032250b81c1c71291d2c770d9ccbb9eacee5a6
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-17 21:00:11 +0300

    Modify auto merge scripts

--- travis-merge-script.sh
+++ travis-merge-script.sh
@@ -10,4 +10,4 @@ git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 git checkout master || exit
 git merge --no-ff -m "Merge pull request #${TRAVIS_PULL_REQUEST} from peterkitonga/${TRAVIS_BRANCH}" "$TRAVIS_COMMIT" || exit
-git push @github.com/">https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git"
+git push "https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git"
