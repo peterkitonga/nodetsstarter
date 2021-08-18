085926188c5e4fc008598d13a73f718aa6d54de8
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-18 05:47:57 +0300

    Add logs to the merge script

--- travis-merge-script.sh
+++ travis-merge-script.sh
@@ -2,15 +2,20 @@
 
 if [ "$TRAVIS_BRANCH" != "develop" ]; then
     exit 0
+    echo "Skipping merge from $TRAVIS_BRANCH..."
 else
+    echo "Configure git credentials..."
     export GIT_COMMITTER_EMAIL="$GITHUB_EMAIL"
     export GIT_COMMITTER_NAME="peterkitonga"
 
-    git remote remove origin
-    git remote set-url origin "https://$GITHUB_TOKEN@github.com/peterkitonga/nodetsstarter.git"
-
+    echo "Fetch refspecs for other branches..."
+    git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
     git fetch origin
+
+    echo "Checkout to master and merge..."
     git checkout master
-    git merge --no-ff -m "Merge pull request #$TRAVIS_PULL_REQUEST from peterkitonga/$TRAVIS_BRANCH" "$TRAVIS_COMMIT"
-    git push origin master
+    git merge --no-ff -m "Merge pull request #${TRAVIS_PULL_REQUEST} from peterkitonga/${TRAVIS_BRANCH}" "$TRAVIS_BRANCH"
+
+    echo "Push merged changes..."
+    git push "https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git"
 fi
