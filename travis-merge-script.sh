4ddd2ac760d53eb4c58d7e5e2b7c76d794f07c2e
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-18 06:16:48 +0300

    Debugging auto merge script

--- travis-merge-script.sh
+++ travis-merge-script.sh
@@ -3,6 +3,7 @@ set -u
 
 if [ "$TRAVIS_BRANCH" != "develop" ]; then
     echo "Pull request ${TRAVIS_PULL_REQUEST}..."
+    echo "Pull request branch ${TRAVIS_PULL_REQUEST_BRANCH}..."
     echo "Skipping merge from ${TRAVIS_BRANCH}..."
     exit 0
 else
@@ -19,5 +20,5 @@ else
     git merge --no-ff -m "Merge pull request #${TRAVIS_PULL_REQUEST} from peterkitonga/${TRAVIS_BRANCH}" "$TRAVIS_COMMIT"
 
     echo "Pushing merged changes..."
-    git push "https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git"
+    git push "https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git" >/dev/null 2>&1
 fi
