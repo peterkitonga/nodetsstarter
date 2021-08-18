2b30f4063ab5fd74bb00b89ac62ec383eea072fc
Peter Kitonga <pkitonga.pk@gmail.com>
2021-08-18 06:03:54 +0300

    Modify auto merge scripts

--- travis-merge-script.sh
+++ travis-merge-script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+set -u
 
 if [ "$TRAVIS_BRANCH" != "develop" ]; then
+    echo "Skipping merge from ${TRAVIS_BRANCH}..."
     exit 0
-    echo "Skipping merge from $TRAVIS_BRANCH..."
 else
-    echo "Configure git credentials..."
+    echo "Configuring git credentials..."
     export GIT_COMMITTER_EMAIL="$GITHUB_EMAIL"
     export GIT_COMMITTER_NAME="peterkitonga"
 
-    echo "Fetch refspecs for other branches..."
+    echo "Fetching refspecs for other branches..."
     git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
     git fetch origin
 
     echo "Checkout to master and merge..."
     git checkout master
-    git merge --no-ff -m "Merge pull request #${TRAVIS_PULL_REQUEST} from peterkitonga/${TRAVIS_BRANCH}" "$TRAVIS_BRANCH"
+    git merge --no-ff -m "Merge pull request #${TRAVIS_PULL_REQUEST} from peterkitonga/${TRAVIS_BRANCH}" "$TRAVIS_COMMIT"
 
-    echo "Push merged changes..."
+    echo "Pushing merged changes..."
     git push "https://${GITHUB_TOKEN}@github.com/peterkitonga/nodetsstarter.git"
 fi
